67e4594655da70ef143748d0508c684e7b27f748
Jannik Gohr <121582314+jannikgohr@users.noreply.github.com>
2026-05-25 22:59:05 +0200

    chore(commitlint): enforce conventional commits

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-empty': [2, 'never'],
+        'scope-case': [2, 'always', 'lower-case'],
+        'type-case': [2, 'always', 'lower-case'],
+    },
+};
